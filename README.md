3565141f061d3466cfc5f72fdfd75804badd92cc
Sathish Sampath <ss.sathishsampath@gmail.com>
2018-06-10 03:33:59 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# WHO News Crawler
+
+Developer: Sathish Sampath(ss.sathishsampath@gmail.com)
+
+## Description
+
+* Developed a crawler to crawl the WHO website for any new News Release. 
+* Store all the data collected as a list of dictionary.
+
+News Alert module, which updates the users about the latest news is yet to integrated.
+
+## Required 
+
+Python : Version 2.7
+
+Libraries:
+
+mechanize, bs4, schedule
+
+
+## Execution
+
+Run the file.
---
+++ crawler_agent.py
@@ -0,0 +1,42 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 10 02:21:35 2018
+
+@author: Sathish Sampath(ss.sathishsampath@gmail.com)
+
+"""
+
+from mechanize import Browser
+from bs4 import BeautifulSoup as BS
+import schedule
+import time
+
+total_news = []
+url = "http://www.who.int/news-room/releases" 
+
+def crawler():
+    global url
+    global total_new
+    agent = Browser()
+    agent.addheaders = [('User-agent', 'Firefox')]
+    agent.set_handle_robots(True)  
+    agent.set_handle_refresh(False) 
+    
+    agent_data = agent.open(url)
+    soup = BS(agent_data.read(), "lxml")
+    
+    for row in soup.find_all('div',attrs={"class" : "list-view--item vertical-list-item"}):   
+        date = row.find('span',attrs={"class" : "timestamp"}).text
+        text = row.find('p',attrs={"class" : "heading text-underline"}).text
+        if {"date": date, "news":text} not in total_news:
+            total_news.append({"date": date, "news": text})
+
+
+schedule.every().day.at("07:30").do(crawler)
+
+
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
